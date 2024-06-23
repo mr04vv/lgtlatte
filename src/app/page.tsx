564c0f49b40aaa91330218777f1532ff8a8521e2
Takuto Mori Gump <mr04vv@gmail.com>
2024-06-23 18:21:35 +0900

    :sparkles: feat: sample Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,37 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { apolloClient } from "@/lib/apolloClient";
+import { LgtmImage } from "./components/LgtmImage";
+import {
+  AssetCollectionDocument,
+  Query,
+  QueryAssetCollectionArgs,
+} from "@/generated/schema";
 
-export default function Home() {
+export default async function Home() {
+  const res = await apolloClient.query<Query, QueryAssetCollectionArgs>({
+    query: AssetCollectionDocument,
+    variables: {
+      skip: 0,
+      limit: 1,
+    },
+  });
+
+  const items = res.data.assetCollection?.items ?? [];
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main>
+      <div>
+        {items.map((item) => (
+          <div
+            style={{
+              display: "block",
+              position: "relative",
+              width: "500px",
+              height: "300px",
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <LgtmImage url={item?.url ?? ""} />
+          </div>
+        ))}
       </div>
     </main>
   );
